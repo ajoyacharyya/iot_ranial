f460e897e12313b830369f7cb5e013f47f184cd5
Ajoy Acharyya <ajoy.acharyya@gmail.com>
2016-04-07 15:33:53 +0530

    Initial Commit

--- com.ranial.parent/com.ranial.core.parent/com.ranial.core.email-api/src/main/java/com/ranial/core/email/api/IEmail.java
+++ com.ranial.parent/com.ranial.core.parent/com.ranial.core.email-api/src/main/java/com/ranial/core/email/api/IEmail.java
@@ -1,9 +1,7 @@
 package com.ranial.core.email.api;
 
-import com.ranial.core.email.model.EmailContract;
-
 public interface IEmail {
 	
-	public void send(EmailContract contract);
+	public void send(String contract);
 
 }
--- com.ranial.parent/com.ranial.core.parent/com.ranial.core.email-impl/src/main/java/com/ranial/core/email/impl/EmailImpl.java
+++ com.ranial.parent/com.ranial.core.parent/com.ranial.core.email-impl/src/main/java/com/ranial/core/email/impl/EmailImpl.java
@@ -4,12 +4,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.ranial.core.email.api.IEmail;
-import com.ranial.core.email.model.EmailContract;
 
 public class EmailImpl implements IEmail{
 	private static final transient Log LOG = LogFactory.getLog(EmailImpl.class);
-	@Override
-	public void send(EmailContract contract) {
+	public void send(String contract) {
 		LOG.info("sending email...");
 		
 	}
