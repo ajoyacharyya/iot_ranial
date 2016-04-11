6621334a88dfa5dcc5d0d018d134b94627272c47
Ajoy Acharyya <ajoy.acharyya@gmail.com>
2016-04-11 21:31:06 +0530

    Multiple child support

--- com.ranial.parent/com.ranial.core.parent/com.ranial.core.mediation-impl/src/main/java/com/ranial/core/mediation/camel/ConvertXMLToMap.java
+++ com.ranial.parent/com.ranial.core.parent/com.ranial.core.mediation-impl/src/main/java/com/ranial/core/mediation/camel/ConvertXMLToMap.java
@@ -14,20 +14,38 @@ import org.xml.sax.InputSource;
 
 public class ConvertXMLToMap {
 	
-	public static Map<String, String> convert(String xmlString) {
-        HashMap<String, String> values = new HashMap<String, String>();
+	public Map<String, Object> convert(String xmlString) {
        // String xmlString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><user><kyc>123</kyc><address>test</address><resiFI>asds</resiFI></user>";
         Document xml = convertStringToDocument(xmlString);
+        HashMap<String, Object> values = new HashMap<String, Object>();
         Node user = xml.getFirstChild();
-        NodeList childs = user.getChildNodes();
-        Node child;
-        for (int i = 0; i < childs.getLength(); i++) {
-            child = childs.item(i);
-            values.put(child.getNodeName(), child.getTextContent());
-        }
+        buildMap(user,values);
         
        return values;
     }
+	
+	private void buildMap(Node node,Map<String,Object> values){
+		
+        NodeList childs = node.getChildNodes();
+        Node child;
+        for (int i = 0; i < childs.getLength(); i++) {
+            child = childs.item(i);
+            /*System.out.println(child.getNodeName());
+            System.out.println(child.getTextContent());*/
+            if(child.hasChildNodes() && !child.getFirstChild().getNodeName().equals("#text")){
+            	String childName = child.getNodeName();
+            	Map<String,Object> childMap = (Map) values.get(childName);
+            	if(childMap==null){
+            		childMap = new HashMap<String,Object>();
+            		values.put(childName, childMap);
+            	}
+            	buildMap(child,childMap);
+            }else{
+            	System.out.println(child.getNodeName() + "value "+child.getTextContent());
+            	values.put(child.getNodeName(), child.getTextContent());
+            }
+        }
+}
 
     private static Document convertStringToDocument(String xmlStr) {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
